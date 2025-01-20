e81e90d9b31e36e2c9e48ef3796ebbd8040fd73e
ZL Asica <40444637+ZL-Asica@users.noreply.github.com>
2025-01-20 16:51:31 -0600

    fix: fix task details dialog date showing

--- src/components/Home/Calendar/TaskDialog.tsx
+++ src/components/Home/Calendar/TaskDialog.tsx
@@ -8,6 +8,17 @@ interface TaskDialogProps {
 }
 
 const TaskDialog = ({ open, selectedTask, handleClose }: TaskDialogProps) => {
+  let actualDateTime = dayjs(selectedTask?.date).format('MMMM D, YYYY')
+
+  if (selectedTask?.timeRange) {
+    const [hour, minute] = selectedTask.timeRange.start.split(':').map(Number)
+    const isPM = hour >= 12
+    const formattedHour = isPM ? hour - 12 || 12 : hour || 12 // Converts 0 to 12 for AM/PM notation
+    const formattedMinute = minute ? `:${minute.toString().padStart(2, '0')}` : ''
+
+    actualDateTime = `${formattedHour}${formattedMinute} ${isPM ? 'PM' : 'AM'}, ${actualDateTime}`
+  }
+
   return (
     <Dialog
       open={open}
@@ -23,16 +34,20 @@ const TaskDialog = ({ open, selectedTask, handleClose }: TaskDialogProps) => {
             <Typography>{selectedTask.title}</Typography>
 
             <Typography variant="subtitle1" fontWeight="bold" mt={2}>
-              Description:
-            </Typography>
-            <Typography>{selectedTask.description}</Typography>
-
-            <Typography variant="subtitle1" fontWeight="bold" mt={2}>
               Date & Time:
             </Typography>
             <Typography>
-              {dayjs(selectedTask.date).format('MMMM D, YYYY, h:mm A')}
+              {actualDateTime}
             </Typography>
+
+            {selectedTask.description !== '' && (
+              <>
+                <Typography variant="subtitle1" fontWeight="bold" mt={2}>
+                  Description:
+                </Typography>
+                <Typography>{selectedTask.description}</Typography>
+              </>
+            )}
           </>
         )}
       </DialogContent>
